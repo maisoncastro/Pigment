0c16edeedf8f4b8c3610d3073d2d46cc11b2b5d4
Nazim-Yoldas Sesen <nazimsesen@gmail.com>
2023-04-24 17:43:57 -0400

    Added image upload aspect

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,34 @@
+import { useState, ChangeEvent } from "react";
 import Head from "next/head";
-import Image from "next/image";
+import NextImage from "next/image";
 import { Inter } from "next/font/google";
 import Navbar from "@/components/Navbar";
+import ColorThief from "colorthief";
+import { CirclePicker } from "react-color";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [palette, setPalette] = useState<string[]>([]);
+
+  const [previewImg, setPreviewImg] = useState<string | null>(null);
+
+  const handleImageUpload = async (event: ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      const previewUrl = URL.createObjectURL(file);
+      setPreviewImg(previewUrl);
+
+      const colorThief = new ColorThief();
+      const img = new Image();
+      img.src = previewUrl;
+      img.onload = () => {
+        const colorPalette = colorThief.getPalette(img, 5); // Adjust the second parameter to get the desired number of colors
+        setPalette(colorPalette.map((color) => `rgb(${color.join(",")})`));
+      };
+    }
+  };
+
   return (
     <>
       <Head>
@@ -19,15 +42,47 @@ export default function Home() {
         <div>
           <span>Extract shades from your photos</span>
           <div>
-            Crave a color combo that syncs with your top pics? BlendedShades'
-            color scheme creator nails it in moments. Just add an image, and
-            we'll craft your palette from its tones.
+            Crave a color combo that syncs with your top pics?
+            BlendedShades&apos; color scheme creator nails it in moments. Just
+            add an image, and we&apos;ll craft your palette from its tones.
           </div>
-          <button>Upload Image</button>
+          <input
+            type="file"
+            accept="image/*"
+            onChange={handleImageUpload}
+            style={{ display: "none" }}
+            id="upload-image"
+          />
+          <label htmlFor="upload-image">
+            <button>Upload Image</button>
+          </label>
+          <button
+            onClick={() => document.getElementById("upload-image")?.click()}
+          >
+            Test Upload Image
+          </button>
         </div>
-        <div></div>
+        <div className="picture-div">
+          {previewImg && (
+            <NextImage
+              src={previewImg}
+              alt="Preview"
+              layout="fill"
+              objectFit="contain"
+            />
+          )}
+        </div>
       </div>
-      <div className="container-palettes"></div>
+      <div className="container-palettes">
+        {palette.length > 0 && (
+          <CirclePicker
+            colors={palette}
+            circleSize={32}
+            circleSpacing={12}
+            width="auto"
+          />
+        )}
+      </div>
     </>
   );
 }
